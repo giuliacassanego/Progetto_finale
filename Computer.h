4c267f90ae9ee2435ca65c2ae34be090b9aad037
angelicazonta <118358702+angelicazonta@users.noreply.github.com>
2023-01-11 11:15:19 +0100

    Update Computer.h

--- Computer.h
+++ Computer.h
@@ -3,20 +3,16 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 
 class Computer
 {
     public:
         Computer();
-        //~Computer();
         
-        int randomChoseNavalUnit(); //funzione per scelta casuale della flotta che deve eseguire il movimento
-        std::string randomChosePosition(); //funzione per scelta casuale della posizione in cui eseguire il movimento
-
+        char randomChoseNavalUnit(); //funzione per scelta casuale della flotta che deve eseguire il movimento
+        //funzioni per la scelta della posizione
+        int randomChosePositionNum();
+        int randomChosePositionLett();
 };
 
 #endif // COMPUTER_H
