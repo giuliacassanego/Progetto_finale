acb1b2bb14cb205981ed7124ca41313434166c73
angelicazonta <118358702+angelicazonta@users.noreply.github.com>
2023-01-11 18:04:13 +0100

    Update GamePlay.h

--- GamePlay.h
+++ GamePlay.h
@@ -2,20 +2,28 @@
 
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
+#include "Player.h"
 
+
+template<typename T> //template che mi rappresenta le tre unità
 class GamePlay
 {
     private:
-       bool win = false; 
+       //bool win = false; 
+       
     
     public:
         GamePlay();
         //~GamePlay();
         
         int start(); //funzione ch decide chi comincia ilioco in modo casuale
-        void checkWin(); //funzione per controllora se è stato affondato il gioco
+        void checkWin(Player p1, Player p2); //funzione per controllora se è stato affondato il gioco
         void playAction();//nome oggetto che deve eseguire il gioco come pratamtro);
-        bool isHit();
+        void affondato(Player p1, T unita);
+        int calcShield(Player p);
+        Player getPlayer1();
+        Player getPlayer2();
 };
 
+#include "GamePlay.hpp"
 #endif // GAMEPLAY_H
