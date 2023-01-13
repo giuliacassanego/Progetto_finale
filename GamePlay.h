34a5eb3bd27a10e38f308ad082cce1e8ce733025
angelicazonta <118358702+angelicazonta@users.noreply.github.com>
2023-01-13 20:45:09 +0100

    Update GamePlay.h

--- GamePlay.h
+++ GamePlay.h
@@ -23,10 +23,10 @@ public:
         
         int start(); //funzione ch decide chi comincia ilioco in modo casuale
         void checkWin(Player p1, Player p2); //funzione per controllora se è stato affondato il gioco
-        void playAction();//nome oggetto che deve eseguire il gioco come pratamtro);
         int calcShield(Player p);
         Player getPlayer1();
         Player getPlayer2();
+        void randomMove();
 };
 
 
